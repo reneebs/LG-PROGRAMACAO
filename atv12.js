4f390ddb3ce1c90ac48d04d9479b24ee10e5ec34
René 'BS <reneborgesantos12@gmail.com>
2026-03-23 21:30:22 -0300

    Implement comparison of two array values

---
+++ atv12.js
@@ -0,0 +1,28 @@
+//Exercicio 2 comparar dois valores do vetor
+  //Crie um vetor com 3 numeros  
+   // //Verifique qual é o maior: o valor da posicao 0 ou da posicao
+
+
+
+let numeros = [1, 2, 3];
+
+numeros[1] = 20;
+
+if (numeros[1] > numeros[2]) {
+    console.log("O valor da posição 1 é maior que o valor da posição 2");
+
+}else if (numeros[1] < numeros[2]) {
+    console.log("O valor da posição 1 é menor que o valor da posição 2");
+
+}else {
+    console.log("O valor da posição 1 é igual ao valor da posição 2");
+}
+
+
+
+
+
+
+
+
+console.log("Professor venha logo, estou com fome e com sono");
