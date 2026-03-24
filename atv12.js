80a70a5ddf583746efd93b9e84d3c5674822b49c
reneebs <aluno.senai@senaiba.com.br>
2026-03-24 19:37:07 -0300

    atualizando atividade

---
+++ atv12.js
@@ -0,0 +1,45 @@
+//Exercicio 2 comparar dois valores do vetor
+  //Crie um vetor com 3 numeros  
+   // //Verifique qual é o maior: o valor da posicao 0 ou da posicao
+
+
+
+let numeros = [1, 2, 3];
+
+numeros[1] = 20;
+
+if (numeros[1] > numeros[2]) {
+    console.log("O valor da posição 1 é maior que o valor da posição 2");
+
+}else if (numeros[1] < numeros[2]) {
+    console.log("O valor da posição 1 é menor que o valor da posição 2");
+
+}else {
+    console.log("O valor da posição 1 é igual ao valor da posição 2");
+}
+
+
+
+
+
+
+
+
+console.log("Professor venha logo, estou com fome e com sono");
+
+
+
+
+
+
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+number [4] = 67
+
+if (number [4] > number[9]) {
+    console.log("O vetor 4, é maior que o vetor 9");
+} else if (number[4] < number[9]) {
+    console.log("O vetor 4, é menor que o vetor 9");
+ } else {
+    console.log("O vetor 4, é igual ao vetor 9"); 
+}
